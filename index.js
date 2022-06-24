da425bace0acf73d928c175ce82b3ab4534ba5c0
Samson Ahadji <samahadji@gmail.com>
2022-06-24 19:07:50 +0000

    Added routes

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+let bodyParser = require('body-parser');
+let validator = require('validator')
 const cors = require('cors');
 const app = express();
+//
+
+let shortUrls = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +15,40 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// Respond to short URL retrieval
+app.get('/api/shorturl/:shorturl', function(req, res) {
+  const shortUrl = req.params.shorturl;
+  if (shortUrl && validator.isInt(shortUrl)) {
+      if (shortUrl <= shortUrls.length) {
+        res.redirect(shortUrls[shortUrl-1])
+      } else {res.json({ error: 'No short URL found for the given input' });}
+  } else {
+    res.json({ error: 'No short URL found for the given input' });
+  }
+
+});
+
+// Respond to short URL add
+app.post('/api/shorturl', function(req, res) {
+  let postedUrl = req.body.url;
+  if (postedUrl && validator.isURL(postedUrl) ) {
+    shortUrls.push(postedUrl)
+    res.json({ original_url: postedUrl, short_url: shortUrls.length });
+
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+  
 });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
